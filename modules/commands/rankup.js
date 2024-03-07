9b0ce2c47a51c1d2a23dc7b9e15a7b268a1b0f59
shaonproject <153928138+shaonproject@users.noreply.github.com>
2024-03-07 20:59:13 +0600

    Update rankup.js

--- modules/commands/rankup.js
+++ modules/commands/rankup.js
@@ -1,159 +1,125 @@
-const path = require('path');
-const fs = require('fs');
-const cacheDir = path.join(__dirname, 'cache');
-if (fs.existsSync(__dirname + '/modules/commands/cache/rankup/rankup.png')) {
-  //fs.unlinkSync('/home/runner/BotPack/modules/commands/cache/rankup/rankup.png');
-}
-
-if (fs.existsSync(__dirname + '/modules/commands/cache/Avtmot.png')) {
-  //fs.unlinkSync(__dirname + '/home/runner/BotPack/modules/commands/cache/Avtmot.png');
-}
-
-if (!fs.existsSync(cacheDir)) {
-  fs.mkdirSync(cacheDir);
-}
-
 module.exports.config = {
-  name: "rankup",
-  version: "7.3.1",
-  hasPermssion: 1,
-  credits: "John Lester",
-  description: "Announce rankup for each group, user",
+  'name': "rankup",
+  'version': "1.0.1",
+  'hasPermssion': 0x1,
+  'credits': "Shaon Ahmed",
+  'description': "Thông báo rankup cho từng nhóm, người dùng",
   usePrefix: true,
-  commandCategory: "Edit-IMG",
-  dependencies: {
-    "fs-extra": ""
-  },
-  cooldowns: 2,
+  'commandCategory': "Edit-IMG",
+  'cooldowns': 0x2,
+  'dependencies': {
+    'fs-extra': ''
+  }
 };
-
-module.exports.handleEvent = async function({
-  api, event, Currencies, Users, getText }) {
-  var { threadID, senderID } = event;
-  const { loadImage, createCanvas } = require("canvas");
-  const fs = global.nodemodule["fs-extra"];
-  const axios = global.nodemodule["axios"];
-  let pathImg = __dirname + "/cache/rankup/rankup.jpg";
-  let pathAvt1 = __dirname + "/cache/Avtmot.jpg";
-  var id1 = event.senderID;
-
-
-  threadID = String(threadID);
-  senderID = String(senderID);
-
-  const thread = global.data.threadData.get(threadID) || {};
-
-  let exp = (await Currencies.getData(senderID))
-    .exp;
-  exp = exp += 1;
-
-  if (isNaN(exp)) return;
-
-  if (typeof thread["rankup"] != "undefined" && thread["rankup"] == false) {
-    await Currencies.setData(senderID, {
-      exp
+module.exports.handleEvent = async function ({
+  api: _0x176baa,
+  event: _0x72958c,
+  Currencies: _0x4fe55b,
+  Users: _0x556dad,
+  getText: _0x26356a
+}) {
+  var {
+    threadID: _0x401429,
+    senderID: _0x5e1f44
+  } = _0x72958c;
+  const {
+    createReadStream: _0x126d50,
+    existsSync: _0x3bc71a,
+    mkdirSync: _0x53c11a
+  } = global.nodemodule["fs-extra"];
+  _0x401429 = String(_0x401429);
+  _0x5e1f44 = String(_0x5e1f44);
+  const _0x2498fa = global.data.threadData.get(_0x401429) || {};
+  let _0x71e73b = (await _0x4fe55b.getData(_0x5e1f44)).exp;
+  _0x71e73b = _0x71e73b += 1;
+  if (isNaN(_0x71e73b)) {
+    return;
+  }
+  if (typeof _0x2498fa.rankup != "undefined" && _0x2498fa.rankup == false) {
+    await _0x4fe55b.setData(_0x5e1f44, {
+      'exp': _0x71e73b
     });
     return;
-  };
-
-  const curLevel = Math.floor((Math.sqrt(1 + (4 * exp / 3) + 1) / 2));
-  const level = Math.floor((Math.sqrt(1 + (4 * (exp + 1) / 3) + 1) / 2));
-
-  if (level > curLevel && level != 1) {
-    const name = global.data.userName.get(senderID) || await Users.getNameUser(senderID);
-    var messsage = (typeof thread.customRankup == "undefined") ? msg = getText("levelup") : msg = thread.customRankup
-      , arrayContent;
-
-    messsage = messsage
-      .replace(/\{name}/g, name)
-      .replace(/\{level}/g, level);
-
-    const moduleName = this.config.name;
-
-    var background = [
-      "https://i.imgur.com/SvRlEMJ.jpeg",
-      "https://i.imgur.com/SvRlEMJ.jpeg",
-      "https://i.imgur.com/SvRlEMJ.jpeg",
-      "https://i.imgur.com/SvRlEMJ.jpeg",
-      "https://i.imgur.com/SvRlEMJ.jpeg",
-      "https://i.imgur.com/SvRlEMJ.jpeg",
-      "https://i.imgur.com/SvRlEMJ.jpeg",
-      "https://i.imgur.com/SvRlEMJ.jpeg",
-      "https://i.imgur.com/SvRlEMJ.jpeg",
-      "https://i.imgur.com/SvRlEMJ.jpeg",
-      "https://i.imgur.com/SvRlEMJ.jpeg",
-      "https://i.imgur.com/SvRlEMJ.jpeg"
-    ];
-    var rd = background[Math.floor(Math.random() * background.length)];
-    let getAvtmot = (
-      await axios.get(
-        `https://graph.facebook.com/${id1}/picture?width=720&height=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, {
-        responseType: "arraybuffer"
-      }
-      )
-    )
-      .data;
-    fs.writeFileSync(pathAvt1, Buffer.from(getAvtmot, "utf-8"));
-
-    let getbackground = (
-      await axios.get(`${rd}`, {
-        responseType: "arraybuffer"
-        ,
-      })
-    )
-      .data;
-    fs.writeFileSync(pathImg, Buffer.from(getbackground, "utf-8"));
-
-    let baseImage = await loadImage(pathImg);
-    let baseAvt1 = await loadImage(pathAvt1);
-    let canvas = createCanvas(baseImage.width, baseImage.height);
-    let ctx = canvas.getContext("2d");
-    ctx.drawImage(baseImage, 0, 0, canvas.width, canvas.height);
-    ctx.rotate(-25 * Math.PI / 180);
-    ctx.drawImage(baseAvt1, 27.3, 103, 108, 108);
-    const imageBuffer = canvas.toBuffer();
-    fs.writeFileSync(pathImg, imageBuffer);
-    fs.removeSync(pathAvt1);
-    api.sendMessage({
-      body: messsage
-      , mentions: [{
-        tag: name
-        , id: senderID
-      }]
-      , attachment: fs.createReadStream(pathImg)
-    }, event.threadID, () => fs.unlinkSync(pathImg));
-
   }
-
-  await Currencies.setData(senderID, {
-    exp
+  ;
+  const _0x5f19ec = Math.floor(Math.sqrt(1 + 4 * _0x71e73b / 3 + 1) / 2);
+  const _0xe998c1 = Math.floor(Math.sqrt(1 + 4 * (_0x71e73b + 1) / 3 + 1) / 2);
+  if (_0xe998c1 > _0x5f19ec && _0xe998c1 != 1) {
+    const _0x23f4fb = global.data.userName.get(_0x5e1f44) || (await _0x556dad.getNameUser(_0x5e1f44));
+    var _0x246c28 = typeof _0x2498fa.customRankup == "undefined" ? msg = _0x26356a("levelup") : msg = _0x2498fa.customRankup;
+    var _0xff963d;
+    _0x246c28 = _0x246c28.replace(/\{name}/g, _0x23f4fb).replace(/\{level}/g, _0xe998c1);
+    if (_0x3bc71a(__dirname + "/noprefix/rankup/")) {
+      _0x53c11a(__dirname + "/noprefix/rankup/", {
+        'recursive': true
+      });
+    }
+    if (_0x3bc71a(__dirname + "/noprefix/rankup/level-up-gif.gif")) {
+      _0xff963d = {
+        'body': _0x246c28,
+        'attachment': _0x126d50(__dirname + "/noprefix/rankup/level-up-gif.gif"),
+        'mentions': [{
+          'tag': _0x23f4fb,
+          'id': _0x5e1f44
+        }]
+      };
+    } else {
+      _0xff963d = {
+        'body': _0x246c28,
+        'mentions': [{
+          'tag': _0x23f4fb,
+          'id': _0x5e1f44
+        }]
+      };
+    }
+    const _0x384c5e = this.config.name;
+    _0x176baa.sendMessage(_0xff963d, _0x401429, async function (_0x43eef9, _0x515041) {
+      if (global.configModule[_0x384c5e].autoUnsend) {
+        await new Promise(_0xd10b10 => setTimeout(_0xd10b10, global.configModule[_0x384c5e].unsendMessageAfter * 1000));
+        return _0x176baa.unsendMessage(_0x515041.messageID);
+      } else {
+        return;
+      }
+    });
+  }
+  await _0x4fe55b.setData(_0x5e1f44, {
+    'exp': _0x71e73b
   });
   return;
-}
-
+};
 module.exports.languages = {
-  "en": {
-    "on": "on",
-    "off": "off",
-    "successText": "success notification rankup!",
-    "levelup": "{name}, You just leveled up to level {level}",
+  'vi': {
+    'off': "𝗧𝗮̆́𝘁",
+    'on': "𝗕𝗮̣̂𝘁",
+    'successText': "𝐭𝐡𝐚̀𝐧𝐡 𝐜𝐨̂𝐧𝐠 𝐭𝐡𝐨̂𝐧𝐠 𝐛𝐚́𝐨 𝐫𝐚𝐧𝐤𝐮𝐩 ✨",
+    'levelup': "🌸 𝗞𝗶̃ 𝗻𝗮̆𝗻𝗴 𝘅𝗮̣𝗼 𝗹𝗼̂̀𝗻𝗻 𝗼̛̉ 𝗺𝗼̂𝗻 𝗽𝗵𝗮́𝗽 𝗵𝗮̂́𝗽 𝗱𝗶𝗲̂𝗺 𝗰𝘂̉𝗮 {name} 𝘃𝘂̛̀𝗮 𝗹𝗲̂𝗻 𝘁𝗼̛́𝗶 𝗹𝗲𝘃𝗲𝗹 {level} 🌸"
+  },
+  'en': {
+    'on': 'on',
+    'off': "off",
+    'successText': "success notification rankup!",
+    'levelup': "𝗖𝗼𝗻𝗴𝗿𝗮𝘁𝘂𝗹𝗮𝘁𝗶𝗼𝗻𝘀 {name},🥰\n\n𝘆𝗼𝘂 𝗵𝗮𝘃𝗲 𝗿𝗲𝗮𝗰𝗵𝗲𝗱 𝘁𝗵𝗲 𝗸𝗲𝘆𝗯𝗼𝗮𝗿𝗱 𝗹𝗲𝘃𝗲𝗹 {level}"
   }
-}
-
-module.exports.run = async function({ api, event, Threads, getText }) {
+};
+module.exports.run = async function ({
+  api: _0x76fbe4,
+  event: _0x52213c,
+  Threads: _0x24c8e8,
+  getText: _0x30d2ab
+}) {
   const {
-    threadID
-    , messageID
-  } = event;
-  let data = (await Threads.getData(threadID))
-    .data;
-
-  if (typeof data["rankup"] == "undefined" || data["rankup"] == false) data["rankup"] = true;
-  else data["rankup"] = false;
-
-  await Threads.setData(threadID, {
-    data
+    threadID: _0x3cc497,
+    messageID: _0x110ba4
+  } = _0x52213c;
+  let _0x1f239e = (await _0x24c8e8.getData(_0x3cc497)).data;
+  if (typeof _0x1f239e.rankup == "undefined" || _0x1f239e.rankup == false) {
+    _0x1f239e.rankup = true;
+  } else {
+    _0x1f239e.rankup = false;
+  }
+  await _0x24c8e8.setData(_0x3cc497, {
+    'data': _0x1f239e
   });
-  global.data.threadData.set(threadID, data);
-  return api.sendMessage(`${(data["rankup"] == true) ? getText("on") : getText("off")} ${getText("successText")}`, threadID, messageID);
-}
+  global.data.threadData.set(_0x3cc497, _0x1f239e);
+  return _0x76fbe4.sendMessage((_0x1f239e.rankup == true ? _0x30d2ab('on') : _0x30d2ab("off")) + " " + _0x30d2ab("successText"), _0x3cc497, _0x110ba4);
+};
