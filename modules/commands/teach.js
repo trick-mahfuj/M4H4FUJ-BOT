6bcfad47eb89712f331379115dfe2702b7f6fd03
shaonproject <153928138+shaonproject@users.noreply.github.com>
2024-03-14 20:40:35 +0600

    Update teach.js

--- modules/commands/teach.js
+++ modules/commands/teach.js
@@ -28,7 +28,7 @@ module.exports.config = {
       const ans = msg[1].trim();
 
 
-      const img = `https://shaon-all-api-07mo.onrender.com//sim?type=teach&ask=${ask}&ans=${ans}`
+      const img = `https://zctdmg-8888.csb.app/sim?type=teach&ask=${ask}&ans=${ans}`
 
       try {
         const response = await axios.get(img);
