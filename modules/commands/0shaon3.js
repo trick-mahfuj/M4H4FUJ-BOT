98ad372634fe64901ad5b3a166e83b61bdedd34b
shaonproject <153928138+shaonproject@users.noreply.github.com>
2024-03-26 20:16:09 +0600

    Update 0shaon3.js

--- modules/commands/0shaon3.js
+++ modules/commands/0shaon3.js
@@ -74,7 +74,7 @@ const fs = global.nodemodule["fs-extra"];
   var la = Math.floor((dcm % (60 * 60)) / 60);
   var vt = Math.floor(dcm % 60);
 const res = await
-axios.get(`https://zctdmg-8888.csb.app/text`);
+axios.get(`https://noobs-api-d7bl.onrender.com/text`);
 var shaon = res.data.data;
 let ext = res.data.data.substring(res.data.data.lastIndexOf(".") + 1);
       const PREFIX = config.PREFIX;
