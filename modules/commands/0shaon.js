a38fbefcbd6c7da0170405254d66f8dede7dfe16
shaonproject <153928138+shaonproject@users.noreply.github.com>
2024-03-27 03:44:20 +0600

    Update 0shaon.js

--- modules/commands/0shaon.js
+++ modules/commands/0shaon.js
@@ -1,39 +1,51 @@
-const axios = require("axios");
-const fs = require("fs");
-const request = require("request");
-const moment = require('moment-timezone');
-const link = [
-  "https://i.imgur.com/SSkDKpQ.mp4"
-];
-
 module.exports.config = {
   name: "☺️",
   version: "1.0.0",
-  hasPermission: 2,
+  hasPermssion: 0,
   credits: "Shaon Ahmed",
+  description: "sand best islamick Video",
   usePrefix: true,
-  description: "when send ,☺️ then response",
-  commandCategory: "no prefix",
+  commandCategory: "noprefix",
   usages: "☺️",
-  cooldowns: 1,
+  cooldowns: 5,
+  dependencies: {
+    "request":"",
+    "fs-extra":"",
+    "axios":""
+  }
+
 };
 module.exports.handleEvent = async ({ api, event, Threads }) => {
-    var time = moment.tz("Asia/Dhaka").format("LLLL");
-  if (event.body.startsWith("☺️")) {
-    const shaon = `•—»✨[ 𝐏𝐫𝐞𝐟𝐢𝐱 𝐄𝐯𝐞𝐧𝐭 ]✨«—•\n•┄┅════❁🌺❁════┅┄•\n\n  __মনের বিষয় টা আল্লাহ ছাড়া কেও জানে নাহ -!!☺️\n\n•┄┅════❁🌺❁════┅┄•\n•—»✨[ 𝐒𝐡𝐚𝐨𝐧 𝐏𝐫𝐨𝐣𝐞𝐜𝐭 ]✨«—•\n[🐰]  𝗣𝗿𝗲𝗳𝗶𝘅 : [ ${PREFIX} ]\n[🫰] 𝐍𝐎𝐏𝐑𝐄𝐅𝐈𝐗  : ☺️\n[⌛]  𝐃𝐚𝐭𝐞  : \n[🍒]  ${global.config.BOTNAME}` //const shaon2 = shaon[Math.floor(Math.random() * shaon.length)];
+    if (event.body.indexOf("☺️")==0 || (event.body.indexOf("☺️")==0) || event.body.indexOf("☺️")==0 ||
+event.body.indexOf("☺️")==0 ||
+event.body.indexOf("☺️")==0 ||
+event.body.indexOf("☺️")==0 ||
+event.body.indexOf("☺️")==0 ||
+event.body.indexOf("☺️")==0 ||
+event.body.indexOf("☺️")==0 ||
+event.body.indexOf("☺️")==0) {
+    const axios = global.nodemodule["axios"];
+const request = global.nodemodule["request"];
+const fs = global.nodemodule["fs-extra"];
+    var link = [
+"https://i.imgur.com/SSkDKpQ.mp4",
+        ];
+     var callback = () => api.sendMessage({body:`•—»✨[ 𝐏𝐫𝐞𝐟𝐢𝐱 𝐄𝐯𝐞𝐧𝐭 ]✨«—•\n•┄┅════❁🌺❁════┅┄•\n\n
+__মনের বিষয় টা আল্লাহ ছাড়া কেও জানে নাহ -!!☺️\n\n•┄┅════❁🌺❁════┅┄•\n•—»✨[ 𝐒𝐡𝐚𝐨𝐧 𝐏𝐫𝐨𝐣𝐞𝐜𝐭 ]✨«—•\n[🐰]  𝗣𝗿𝗲𝗳𝗶𝘅 : [ ${PREFIX} ]\n[🫰] 𝐍𝐎𝐏𝐑𝐄𝐅𝐈𝐗  : ☺️\n[⌛]  𝐃𝐚𝐭𝐞  : ${Date.now() - timeStart} 𝐓𝐢𝐦𝐞\n[🍒]  ${global.config.BOTNAME}`,attachment: fs.createReadStream(__dirname + "/cache/2024.mp4")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/2024.mp4"), event.messageID);
+  const timeStart = Date.now();
+  const dcm = process.uptime(); 
+ var anh = Math.floor(dcm / (60 * 60));
+  var la = Math.floor((dcm % (60 * 60)) / 60);
+  var vt = Math.floor(dcm % 60);
+const res = await
+axios.get(`https://noobs-api-d7bl.onrender.com/text`);
+var shaon = res.data.data;
+let ext = res.data.data.substring(res.data.data.lastIndexOf(".") + 1);
+      const PREFIX = config.PREFIX;
+      return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/2024.mp4")).on("close",() => callback());
+}
 
-    const callback = () => api.sendMessage({
-      body: `${shaon}`,
-      attachment: fs.createReadStream(__dirname + "/cache/2025.mp4")
-    }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/2025.mp4"), event.messageID);
-    
-    const requestStream = request(encodeURI(link[Math.floor(Math.random() * link.length)]));
-    requestStream.pipe(fs.createWriteStream(__dirname + "/cache/2025.mp4")).on("close", () => callback());
-    return requestStream;
-  }
-};
-
-module.exports.languages = {
+  module.exports.languages = {
   "vi": {
     "on": "Dùng sai cách rồi lêu lêu",
     "off": "sv ngu, đã bão dùng sai cách",
@@ -44,14 +56,24 @@ module.exports.languages = {
     "off": "off",
     "successText": "success!",
   }
-};
-
-module.exports.run = async ({ api, event, Threads, getText }) => {
-  const { threadID, messageID } = event;
+    }
+  module.exports.run = async ({ event, api, Threads, getText }) => {
+  let { threadID, messageID } = event;
   let data = (await Threads.getData(threadID)).data;
-  if (typeof data["☺️"] === "undefined" || data["☺️"]) data["☺️"] = false;
+  if (typeof data["☺️"] == "undefined" || data["☺️"] == true) data["☺️"] = false;
   else data["☺️"] = true;
-  await Threads.setData(threadID, { data });
+  await Threads.setData(threadID, {
+    data
+  });
   global.data.threadData.set(threadID, data);
-  api.sendMessage(`${(data["☺️"]) ? getText("off") : getText("on")} ${getText("successText")}`, threadID, messageID);
-};
+api.sendMessage(`${(data["☺️"] == false) ? getText("off") : getText("on")} ${getText("successText")}`, threadID, messageID);
+}      
+
+
+
+
+
+    }
+module.exports.run = async({api,event,args,Users,Threads,Currencies}) => {
+
+   };
