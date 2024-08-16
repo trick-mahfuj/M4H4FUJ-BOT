abeb50f1c3fd9ba94a88315b59551af46000c1fc
shaonproject <153928138+shaonproject@users.noreply.github.com>
2024-08-16 11:03:11 +0600

    Update imgur.js

--- modules/commands/imgur.js
+++ modules/commands/imgur.js
@@ -17,7 +17,7 @@ module.exports.run = async ({ api, event }) => {
 const axios = global.nodemodule['axios'];  
 var linkanh = event.messageReply.attachments[0].url || args.join(" ");
   if(!linkanh) return api.sendMessage('╭•┄┅════❁SHAON PROJECT❁════┅┄•╮\n\n আসসালামু আলাইকুম-!!🖤💫\n আপনি যেই ছোবি টাকে Imgur link বানাতে চান সেই ছোবি টা imgur লিখে রিপ্লাই করুন \n\n╰•┄┅════❁SHAON PROJECT❁════┅┄•╯', event.threadID, event.messageID)
-const res = await axios.get(`https://nazrul-apis.onrender.com/imgurv2?link=${encodeURIComponent(linkanh)}`);    
+const res = await axios.get(`https://all-api-ius8.onrender.com/imgurv2?link=${encodeURIComponent(linkanh)}`);    
 var img = res.data.uploaded.image;
   return api.sendMessage(`"${img}",`, event.threadID, event.messageID);
 }
